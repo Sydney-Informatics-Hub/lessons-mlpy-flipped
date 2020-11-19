8c45cb5bdd862f264f3b9fa3a7542454f7d9090e
Sergio <sergio.pintaldi@gmail.com>
2020-11-19 14:06:29 +1100

    move fix for pandas table to extra.js

--- course/theme/js/config.js
+++ course/theme/js/config.js
@@ -10,8 +10,3 @@ window.MathJax = {
     processHtmlClass: "arithmatex"
   }
 };
-
-// Fix pandas tables overflow by adding an horizontal scroll
-Array.from(document.getElementsByTagName("table")).forEach( function(item) {
-  item.parentElement.style.overflowX = "auto";
-})
