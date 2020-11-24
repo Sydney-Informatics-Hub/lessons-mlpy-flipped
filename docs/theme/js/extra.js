aaf7cc7a8bf9cbada299196834e96e4c9cf321ae
Sergio <sergio.pintaldi@gmail.com>
2020-11-24 21:11:43 +1100

    removed deploying folder, added to gitignore

--- docs/theme/js/extra.js
+++
@@ -1,4 +0,0 @@
-// Fix pandas tables overflow by adding an horizontal scroll
-Array.from(document.getElementsByTagName("table")).forEach( function(item) {
-  item.parentElement.style.overflowX = "auto";
-})
