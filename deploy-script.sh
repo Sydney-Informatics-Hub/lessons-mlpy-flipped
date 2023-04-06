25148cd2ff73c6ce5aefa7ebe020191d597ac17d
Marius Mather <marius.mather@sydney.edu.au>
2023-04-06 14:08:05 +1000

    Remove the WEBSITERENDERING variable, just creates mess in the notebook code

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,7 +1,7 @@
 echo '*************************************'
 date
 echo 'Executing all the notebooks . . .'
-WEBSITERENDERING="False" jupyter nbconvert ./course/notebooks/*.ipynb --to notebook --execute --clear-output
+jupyter nbconvert ./course/notebooks/*.ipynb --to notebook --execute --clear-output
 # echo '------'
 # echo 'Committing the course folder new changes . . .'
 # git add -f ./course/*
